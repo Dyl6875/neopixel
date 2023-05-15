246ecb5504a92bdfca36c64ec7075011e2c1a97f
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-15 10:12:39 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
-strip.setBrightness(15)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setBrightness(5)
+strip.setPixelColor(0, neopixel.rgb(0, 255, 255))
 strip.show()
 basic.forever(function () {
     basic.pause(100)
